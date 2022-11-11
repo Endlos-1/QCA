687fc6862d4e0cb52539c26b8f3166a8aef4c7a7
Mahak Bhansali <115609529+Endlos-1@users.noreply.github.com>
2022-11-11 15:03:18 +0530

    sfdfd

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import Agent,AmountRequest,Property,AgentBankDetails,ContactUs
 
-# Register your models here.
+admin.site.register(Agent)
+admin.site.register(AmountRequest)
+admin.site.register(Property)
+admin.site.register(AgentBankDetails)
+admin.site.register(ContactUs)
+
+
