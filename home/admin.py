e9c631a7bfd081af00422e1c6e7ce7e04bc4ace8
Mahak Bhansali <115609529+Endlos-1@users.noreply.github.com>
2022-11-14 17:07:35 +0530

    www

--- home/admin.py
+++ home/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.contrib import admin
-from .models import Agent,AmountRequest,Property,AgentBankDetails,ContactUs
+from .models import Agent,AmountRequest,Property,AgentBankDetails,ContactUs,SalesInformation_AgentDetail,RemainingFormRecords,ClosingCompanyDetails
 
 admin.site.register(Agent)
 admin.site.register(AmountRequest)
 admin.site.register(Property)
 admin.site.register(AgentBankDetails)
 admin.site.register(ContactUs)
+admin.site.register(SalesInformation_AgentDetail)
+admin.site.register(RemainingFormRecords)
+admin.site.register(ClosingCompanyDetails)
 
 
