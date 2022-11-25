d846212d9a32fbe1d8c60bd5b7827547d12c2d33
Mahak Bhansali <115609529+Endlos-1@users.noreply.github.com>
2022-11-25 11:45:45 +0530

    again change

--- fundraiser/settings.py
+++ fundraiser/settings.py
@@ -146,7 +146,7 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "info@quickcommissionadvance.com"
-EMAIL_HOST_PASSWORD = "$$Chase88"
+EMAIL_HOST_PASSWORD = "mbuxwckpnzjccvsw"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
